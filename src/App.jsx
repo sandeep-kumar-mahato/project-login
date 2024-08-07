1eb3a1b2e73f9da6bf90069e42f145fbf6506d1e
sandeep kumar mahato <sandeepkumarmahato712@gmail.com>
2024-08-07 17:00:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import SignInSignUp from "./components/SignInSignUp";
+
+const App = () => {
+  return (
+    <div>
+      <h1></h1>
+      <SignInSignUp />
+    </div>
+  );
+};
+
+export default App;
